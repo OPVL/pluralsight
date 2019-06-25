34fa81e01a69564bd1937669998f7fe3677c4eff
Lloyd <lloydculpepper4@gmail.com>
2019-06-25 14:34:24 +0100

    getter n setter

--- javascript/Objects and Prototypes/script.js
+++ javascript/Objects and Prototypes/script.js
@@ -5,9 +5,27 @@ let cat = {
     colour: 'Blue'
 }
 
-cat['Leg Length'] = 4;
+Object.defineProperty(cat, 'fullName', {
+    get: function(){
+        return `${this.name.first} ${this.name.last}`;
+    },
+    set: function(value){
+        let nameParts = value.split(' ');
+        this.name.first = nameParts[0];
+        this.name.last = nameParts[1];
+    }
+});
 
-Object.defineProperty(cat, 'name', {configurable: false});
-Object.defineProperty(cat, 'name', {writable: false}); // can change writable
-Object.defineProperty(cat, 'name', {enumerable: false}); // cannot redefine property & prevents deletion
-delete cat.name;
+display(cat.fullName);
+cat.fullName = 'Garmanar Felds';
+display(cat.fullName);
+{
+// cat['Leg Length'] = 4;
+
+// // Object.defineProperty(cat, 'name', {configurable: false});
+// Object.defineProperty(cat, 'name', {writable: false}); // can change writable
+// Object.defineProperty(cat, 'name', {enumerable: false}); // cannot redefine property & prevents deletion
+// delete cat.name;
+
+// display(cat);
+}
