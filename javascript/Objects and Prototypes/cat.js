78f6258cee607597aadb528954f127fd6fb8331d
Lloyd <lloydculpepper4@gmail.com>
2019-06-25 14:05:23 +0100

    Object.create syntax (fuck that ES2015+ ftw)

--- javascript/Objects and Prototypes/cat.js
+++ javascript/Objects and Prototypes/cat.js
@@ -1,11 +1,26 @@
 'use strict';
 
 //new
-function Cat(name, colour){
-    this.name = name
-    this.colour = colour
-}
+// function Cat(name, colour){
+//     this.name = name
+//     this.colour = colour
+// }
 
-let cat = new Cat('Kevin', 'Disgruntled Muffin');
+// let cat = new Cat('Kevin', 'Disgruntled Muffin');
+
+let cat = Object.create(Object.prototype,{
+    name: {
+        value: 'Fluffy',
+        enumerable: true,
+        writable: true,
+        configurable: true
+    },
+    color: {
+        value: 'White',
+        enumerable: true,
+        writable: true,
+        configurable: true
+    }
+});
 
 display(cat);
