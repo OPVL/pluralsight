35f84b4565ee53a9d0f6f023eba67262b609b19f
Lloyd <lloydculpepper4@gmail.com>
2019-06-25 14:26:33 +0100

    property configurable fun

--- javascript/Objects and Prototypes/script.js
+++ javascript/Objects and Prototypes/script.js
@@ -7,23 +7,6 @@ let cat = {
 
 cat['Leg Length'] = 4;
 
-// fuckin skitz
-Object.defineProperty(cat, 'name', {writable: false});
-// Object.freeze(cat.name);
-cat.name.first = 'Greg';
-display(Object.getOwnPropertyDescriptor(cat, 'name'));
-display(cat.name);
-
-Object.defineProperty(cat, 'name', {enumerable: false});
-display(Object.getOwnPropertyDescriptor(cat, 'name'));
-
-for (const propertyName in cat) {
-    if (cat.hasOwnProperty(propertyName)) {
-        const property = cat[propertyName];
-        display(property);
-    }
-}
-
-display(Object.keys(cat));
-
-display(JSON.stringify(cat)); // enumerable prevents the property from being serialised in to JSON
+Object.defineProperty(cat, 'name', {configurable: false});
+Object.defineProperty(cat, 'name', {enumerable: false}); // cannot redefine property & prevents deletion
+delete cat.name;
