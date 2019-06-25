aceb04ac695d5e01c6fbc561f2beb325271fb748
Lloyd <lloydculpepper4@gmail.com>
2019-06-25 13:39:44 +0100

    garmanafield

--- javascript/Objects and Prototypes/script.js
+++ javascript/Objects and Prototypes/script.js
@@ -4,5 +4,10 @@ let cat = {
     name: 'Fluffy',
     color: 'White'
 }
+cat.age = 4;
+cat.speak = function () {
+    display("Where's my lasagna John?");
+}
 
 display(cat.name);
+display(cat.age);
