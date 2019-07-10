3320989846cf0729a000c0f2a972d779266dbaca
Lloyd <lloydculpepper4@gmail.com>
2019-07-10 16:00:32 +0100

    updated

---
+++ javascript/Progressive Web Apps/scratch/dist/precache-manifest.1bc0a1071914e6e57990f7668f9133db.js
@@ -0,0 +1,13 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416f808c040263b2e4b6c2877c40a221",
+    "url": "assets/logo.svg"
+  },
+  {
+    "revision": "d9e8fe0ea11d684f01fecf00a167228f",
+    "url": "index.html"
+  },
+  {
+    "url": "main.3a80b66c44e760edb8d2.js"
+  }
+]);
